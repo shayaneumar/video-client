a03d03d6f0c7d8896eac3a0f190f87e40c97691c
Shayane Umar <shayane.umar@gmail.com>
2018-11-16 16:18:35 +0530

    Add unit tests of vidyo-client.ts

---
+++ projects/video-client/src/lib/vidyo/vidyo-client.spec.ts
@@ -0,0 +1,136 @@
+import { VidyoClient } from './vidyo-client';
+import { VidyoClientState } from '.';
+
+describe('VidyoClient', () => {
+    describe('connect', () => {
+        it('returns the saved state when vidyo client is already initialize', () => {
+            // Arrange
+            const vidyoClient = new VidyoClient();
+            vidyoClient.vidyoClientState = <VidyoClientState>{ state: 'READY' };
+
+            // Act + Assert
+            vidyoClient.connect({ webrtc: true, plugin: false }).then(vidyoClientState => {
+                expect(vidyoClientState.state).toBe('READY');
+            });
+        });
+    });
+
+    describe('getConnectState', () => {
+        it('returns status of vidyo client', () => {
+            // Arrange
+            const vidyoClient = new VidyoClient();
+            vidyoClient.vidyoClientState = <VidyoClientState>{ state: 'READY' };
+
+            // Act + Assert
+            vidyoClient.getConnectState().then(vidyoClientState => {
+                expect(vidyoClientState.state).toBe('READY');
+            });
+        });
+    });
+
+    describe('createVidyoConnector', () => {
+        it('creates a new connector object', () => {
+            // Arrange
+            const vidyoClient = new VidyoClient();
+            vidyoClient.vidyoClient = jasmine.createSpyObj('vidyoClient', {
+                CreateVidyoConnector: Promise.resolve({})
+            });
+
+            // Act + Assert
+            vidyoClient
+                .createVidyoConnector({
+                    viewId: null,
+                    viewStyle: 'VIDYO_CONNECTORVIEWSTYLE_Default',
+                    remoteParticipants: 25,
+                    logFileFilter: undefined,
+                    logFileName: undefined,
+                    userData: null
+                })
+                .then(() => {
+                    expect(vidyoClient.vidyoConnector).toBeDefined();
+                });
+        });
+
+        it('returns connector which is already defined', () => {
+            // Arrange
+            const vidyoClient = new VidyoClient();
+            const vidyoConnector = <any>{ Connect: Function };
+            vidyoClient.vidyoConnector = vidyoConnector;
+
+            // Act + Assert
+            vidyoClient
+                .createVidyoConnector({
+                    viewId: null,
+                    viewStyle: 'VIDYO_CONNECTORVIEWSTYLE_Default',
+                    remoteParticipants: 25,
+                    logFileFilter: undefined,
+                    logFileName: undefined,
+                    userData: null
+                })
+                .then(vc => expect(vc).toBe(vidyoConnector));
+        });
+    });
+
+    describe('getClientStatusGlobal', () => {
+        it('returns status vidyo client state', () => {
+            // Arrange
+            const vidyoClient = new VidyoClient();
+            vidyoClient.vidyoClientState = <VidyoClientState>{ state: 'READY' };
+
+            // Act + Assert
+            vidyoClient.getClientStatusGlobal().then(vidyoClientState => {
+                expect(vidyoClientState.state).toBe('READY');
+            });
+        });
+
+        it('returns error with state FAILED', () => {
+            // Arrange
+            const vidyoClient = new VidyoClient();
+            window['VCUtils'] = { params: { webrtc: true } };
+
+            // Act + Assert
+            vidyoClient.getClientStatusGlobal().catch(error => {
+                expect(error).toBe('error');
+            });
+
+            vidyoClient.vidyoClientReadyStateChanged({ state: 'FAILED', description: 'error' });
+        });
+
+        it('returns error with state FAILEDVERSION', () => {
+            // Arrange
+            const vidyoClient = new VidyoClient();
+            window['VCUtils'] = { params: { webrtc: true } };
+
+            // Act + Assert
+            vidyoClient.getClientStatusGlobal().catch(error => {
+                expect(error).toBe('error');
+            });
+
+            vidyoClient.vidyoClientReadyStateChanged({ state: 'FAILEDVERSION', description: 'error' });
+        });
+    });
+
+    describe('vidyoClientReadyStateChanged', () => {
+        it('sets vidyo client state', () => {
+            // Arrange
+            const vidyoClient = new VidyoClient();
+            window['VCUtils'] = { params: { webrtc: true } };
+            vidyoClient.getClientStatusGlobal();
+
+            // Act
+            vidyoClient.vidyoClientReadyStateChanged({ state: 'a-state' });
+
+            // Assert
+            expect(vidyoClient.vidyoClientState.state).toBe('a-state');
+        });
+
+        it('returns true', () => {
+            // Arrange
+            const vidyoClient = new VidyoClient();
+            window['VCUtils'] = { params: { webrtc: true } };
+
+            // Act + Assert
+            expect(vidyoClient.vidyoClientReadyStateChanged({})).toBeTruthy();
+        });
+    });
+});
--- projects/video-client/src/lib/vidyo/vidyo-client.ts
+++ projects/video-client/src/lib/vidyo/vidyo-client.ts
@@ -1,5 +1,5 @@
 import { VidyoClientState, VidyoConnector, VidyoClientInterface, ViewStyle, ScriptModel } from '.';
-import { Subject , Subscription } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
 
 const VC_StatusSubject = new Subject<VidyoClientState>();
 
@@ -7,47 +7,45 @@ export class VidyoClient {
     private VIDYOCLIENT = 'https://static.vidyo.io/4.1.24.8/javascript/VidyoClient/VidyoClient.js';
     private _scripts: ScriptModel[] = [];
 
-    private _vidyoConnector: VidyoConnector;
-    private _vidyoClientState: VidyoClientState;
-    private _vidyoClient: VidyoClientInterface;
+    vidyoConnector: VidyoConnector;
+    vidyoClientState: VidyoClientState;
+    vidyoClient: VidyoClientInterface;
 
-    constructor() { }
+    constructor() {}
 
-    public connect(options: { webrtc: boolean, plugin: boolean }): Promise<VidyoClientState> {
-        if (this._vidyoClientState) {
-            return Promise.resolve(this._vidyoClientState);
+    public connect(options: { webrtc: boolean; plugin: boolean }): Promise<VidyoClientState> {
+        if (this.vidyoClientState) {
+            return Promise.resolve(this.vidyoClientState);
         } else {
             return this.loadVidyoClientScript(options);
         }
     }
 
     public getConnectState(): Promise<VidyoClientState> {
-        return Promise.resolve(this._vidyoClientState);
+        return Promise.resolve(this.vidyoClientState);
     }
 
     public createVidyoConnector(options: {
-        viewId: string | null,
-        viewStyle: ViewStyle,
-        remoteParticipants: number,
-        logFileFilter: string,
-        logFileName: string,
-        userData: number
+        viewId: string | null;
+        viewStyle: ViewStyle;
+        remoteParticipants: number;
+        logFileFilter: string;
+        logFileName: string;
+        userData: number;
     }): Promise<VidyoConnector> {
-
         console.log('vidyo.createVidyoConnector', options);
 
-        if (!this._vidyoConnector) {
-            return this._vidyoClient.CreateVidyoConnector(options)
-                .then(_response => {
-                    this._vidyoConnector = _response;
-                    return this._vidyoConnector;
-                });
+        if (!this.vidyoConnector) {
+            return this.vidyoClient.CreateVidyoConnector(options).then(_response => {
+                this.vidyoConnector = _response;
+                return this.vidyoConnector;
+            });
         } else {
-            return Promise.resolve(this._vidyoConnector);
+            return Promise.resolve(this.vidyoConnector);
         }
     }
 
-    private loadVidyoClientScript(options: { webrtc: boolean, plugin: boolean }): Promise<VidyoClientState> {
+    private loadVidyoClientScript(options: { webrtc: boolean; plugin: boolean }): Promise<VidyoClientState> {
         console.log('vidyo.loadVidyoClientScript()', options);
 
         const webrtc = options.plugin ? false : true;
@@ -72,16 +70,13 @@ export class VidyoClient {
             })
             .then(_state => {
                 console.log('vidyo.loadVidyoClientScript(): vidyoClient is ready');
-                this._vidyoClientState = _state;
-                return this._vidyoClientState;
+                this.vidyoClientState = _state;
+                return this.vidyoClientState;
             });
-
     }
 
     private loadScript(script: ScriptModel): Promise<ScriptModel> {
-
         return new Promise<ScriptModel>((resolve, reject) => {
-
             const existingScript = this._scripts.find(s => {
                 return s.name === script.name;
             });
@@ -110,24 +105,23 @@ export class VidyoClient {
         });
     }
 
-    private getClientStatusGlobal(): Promise<VidyoClientState> {
-
+    getClientStatusGlobal(): Promise<VidyoClientState> {
         return new Promise((resolve, reject) => {
-            if (this._vidyoClientState) {
-                resolve(this._vidyoClientState);
+            if (this.vidyoClientState) {
+                resolve(this.vidyoClientState);
             } else {
                 console.log('vidyo.getClientStatusGlobal(): waiting for window.VC to become available');
                 let subscription: Subscription;
 
-                subscription = VC_StatusSubject.subscribe((_status) => {
+                subscription = VC_StatusSubject.subscribe(_status => {
                     console.log('vidyo.getClientStatusGlobal(): status is available', _status);
-                    this._vidyoClient = (window as any).VC;
-                    this._vidyoClientState = _status;
+                    this.vidyoClient = (window as any).VC;
+                    this.vidyoClientState = _status;
 
                     switch (_status.state) {
-                        case 'READY':    // The library is operating normally
+                        case 'READY': // The library is operating normally
                             subscription.unsubscribe();
-                            resolve(this._vidyoClientState);
+                            resolve(this.vidyoClientState);
                             break;
                         case 'NOTAVAILABLE': // can happen on plugin loading
                         case 'RETRYING': // The library operating is temporarily paused
@@ -135,19 +129,18 @@ export class VidyoClient {
                             console.warn(`vidyo.getClientStatusGlobal(): ${_status.state} Temporarily unavailable retrying in
                                          ${timeOutSeconds} seconds`);
                             break;
-                        case 'FAILED':   // The library operating has stopped
-                        case 'FAILEDVERSION':   // The library operating has stopped
+                        case 'FAILED': // The library operating has stopped
+                        case 'FAILEDVERSION': // The library operating has stopped
                             console.error(`vidyo.getClientStatusGlobal(): failure=> ${_status.state} ${_status.description}`);
                             subscription.unsubscribe();
                             reject(_status.description);
-
                     }
                 });
             }
         });
     }
 
-    private vidyoClientReadyStateChanged(status) {
+    vidyoClientReadyStateChanged(status) {
         // this is the global handler that is triggered when the script is loaded
         // it may be triggered more than once in the case of a plugin
         console.log('vidyo.vidyoClientReadyStateChanged', status);
